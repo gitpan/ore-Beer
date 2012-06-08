cc585cb770b43d32b5bff11de665833491cbafb7
Jonathan Leto <jonathan@leto.net>
2012-06-08 23:52:32 +0000

    Import of LETO/ore-Beer-0.02 from CPAN.
    gitpan-cpan-distribution: ore-Beer
    gitpan-cpan-version:      0.02
    gitpan-cpan-path:         LETO/ore-Beer-0.02.tar.gz
    gitpan-cpan-author:       LETO
    gitpan-cpan-maturity:     released

--- lib/ore/Beer.pm
+++ lib/ore/Beer.pm
@@ -3,21 +3,17 @@ package ore::Beer;
 use warnings;
 use strict;
 
-
-
-
 =head1 NAME
 
 ore::Beer - Add Beer To Perl With Ease
 
 =head1 VERSION
 
-Version 0.01
+Version 0.02
 
 =cut
 
-our $VERSION = '0.01';
-
+our $VERSION = '0.02';
 
 =head1 SYNOPSIS
 
@@ -29,7 +25,7 @@ Jonathan "Duke" Leto, C<< <duke@leto.net> >>
 
 =head1 DEDICATION
 
-This module is lovingly dedicated to PDX Hackathon
+This module is lovingly dedicated to PDX.pm and PDX Hackathon
 L<https://twitter.com/#!/pdxhackathon>. Keep the fine Portland brews flowing.
 
 =head1 BUGS
